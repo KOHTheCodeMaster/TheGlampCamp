80ec0d49e7f4644aed16ee36070e191468655d73
Harshit Gupta <kohthecodemaster@gmail.com>
2020-12-14 21:51:10 +0530

    8th Commit - Add New Comments 1. Refactored views directory & all the associated ejs files 2. Add new comments
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ let express = require("express"),
     mongoose = require("mongoose"),
     bodyparser = require("body-parser"),
     Campground = require("./models/campground"),
-    // Comment = require("./models/comment"),
+    Comment = require("./models/comment"),
     seedDB = require("./seedDB"),
     app = express();
 
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 //  --------------------------------------------------
 
 //  Establish Connection with DB
-mongoose.connect("mongodb://localhost:27017/TheGlampCamp_v1_0_4", function (err) {
+mongoose.connect("mongodb://localhost:27017/TheGlampCamp_v1_0_5", function (err) {
     if (err) console.log(err);
     else console.log("Connection To MongoDB Established successfully.")
 });
@@ -31,7 +31,7 @@ seedDB();
 
 //  Home page route
 app.get("/", function (req, res) {
-    res.render("home");
+    res.render("campground/home");
 });
 
 // INDEX - Campgrounds index page route
@@ -44,7 +44,7 @@ app.get("/index", function (req, res) {
             return;
         }
         //  Render all the campgrounds fetched from DB
-        res.render("index", {campgrounds: campgrounds});
+        res.render("campground/index", {campgrounds: campgrounds});
     });
 
 });
@@ -61,7 +61,7 @@ app.post("/index", function (req, res) {
         else {
             console.log("New Campground Added." + campground);
             //  Redirect to /index
-            res.redirect("index");
+            res.redirect("campground/index");
         }
     });
 
@@ -69,7 +69,7 @@ app.post("/index", function (req, res) {
 
 //  NEW - form for new campground
 app.get("/index/new", function (req, res) {
-    res.render("new");
+    res.render("campground/new");
 });
 
 //  SHOW - particular campground by id
@@ -78,8 +78,47 @@ app.get("/index/:id", function (req, res) {
     Campground.findById(req.params.id).populate("comments")
         .exec(function (err, campground) {
             if (err) console.log(err);
-            else res.render("show", {campground: campground});
+            else res.render("campground/show", {campground: campground});
         });
+
+});
+
+//  ==============================
+//  COMMENTS Routes
+//  ==============================
+
+//  NEW - form for new comment to be added to specific campground by id
+app.get("/index/:id/comments/new", function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) console.log(err);
+        else res.render("comment/new", {campground: campground});
+    });
+});
+
+//  POST - new comment to the campground by id
+app.post("/index/:id/comments", function (req, res) {
+
+    //  Find the specific campground by id
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        //  Create the comment
+        Comment.create(req.body.comment, function (err, comment) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            //  Add the comment to the campground
+            campground.comments.push(comment);
+            campground.save();
+
+            //  Redirect back to the respective campground
+            res.redirect("/index/" + campground._id);
+        });
+
+    });
 
 });
 
